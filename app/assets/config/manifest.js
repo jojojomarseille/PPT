632f092d67b52066d712dfe72651b55a908c949b
Joachim Bourquardez <j.borquardez@laposte.net>
2023-11-10 20:45:08 +0000

    adding nav bar and card

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,10 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
--- app/controllers/piscines_controller.rb
+++ app/controllers/piscines_controller.rb
@@ -1,13 +1,28 @@
 class PiscinesController < ApplicationController
   def show
+    @piscine = Piscine.find(params[:id])
   end
 
   def index
+    @piscines = Piscine.all
   end
 
   def create
+    @piscine = Piscine.new(piscine_params)
+    if @piscine.save
+      redirect_to @piscine
+    else
+      render 'new'
+    end
   end
 
   def new
+    @piscine = Piscine.new
+  end
+
+private
+ 
+  def piscine_params
+    params.require(:piscine).permit(:name, :adress, :cp, :ville, :pays, :mail, :tel, :user, :mondaystart, :mondayend, :tuesdaystart, :tuesdayend, :wednesdaystart, :wednesdayend, :thursdaystart, :thursdayend, :fridaystart, :fridayend, :saturdaystart, :saturdayend, :sundaystart, :sundayend)
   end
 end
