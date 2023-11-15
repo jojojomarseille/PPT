640754d9ff1a7a7a71e9f3508c4acb87fdb69c5b
Joachim Bourquardez <j.borquardez@laposte.net>
2023-11-15 18:41:12 +0000

    add destroy of a piscine

--- app/controllers/piscines_controller.rb
+++ app/controllers/piscines_controller.rb
@@ -27,6 +27,53 @@ class PiscinesController < ApplicationController
     @piscine = Piscine.new
   end
 
+  def destroy
+    # load existing object again from URL param
+    piscine = Piscine.find(params[:id])
+    # destroy object
+    piscine.destroy
+    # respond_to block
+    respond_to do |format|
+        format.html do
+            # success message
+            flash[:success] = 'Piscine removed successfully'
+            # redirect to index
+            redirect_to root_path
+        end
+    end
+end
+
+# def edit
+#   question = McQuestion.find(params[:id])
+#   respond_to do |format|
+#       format.html { render :edit, locals: { question: question } }
+#   end
+# end
+
+# def update
+#   # load existing object again from URL param
+#   question = McQuestion.find(params[:id])
+#   # respond_to block
+#   respond_to do |format|
+#       format.html do
+#           if question.update(params.require(:mc_question).permit(:question, :answer, :distractor_1, :distractor_2))
+#               # success message
+#               flash[:success] = 'Question updated successfully'
+#               # redirect to index
+#               redirect_to quiz_url(question.quiz)
+#           else
+#               # error message
+#               flash.now[:error] = 'Error: Question could not be updated'
+#               # render edit
+#               render :edit, locals: { question: question }
+#           end
+#       end
+#   end
+# end
+
+
+
+
 private
  
   def piscine_params
